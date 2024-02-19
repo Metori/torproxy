4dfbd0dd93ad10f916dea34eeb693a7656ab9ec9
Metori <artem092@mail.ru>
2024-02-19 11:12:30 +1000

    fix

--- torproxy.sh
+++ torproxy.sh
@@ -128,6 +128,7 @@ shift $(( OPTIND - 1 ))
             $(sed 's/^/"/; s/$/"/; s/;/" "/g' <<< $SERVICE)
 [[ "${USERID:-""}" =~ ^[0-9]+$ ]] && usermod -u $USERID -o tor
 [[ "${GROUPID:-""}" =~ ^[0-9]+$ ]] && groupmod -g $GROUPID -o tor
+IFS=$'\n'
 for env in $(printenv | grep '^TOR_'); do
     name="$(cut -c5- <<< ${env%%=*})"
     len=$((${#name} + 6))
